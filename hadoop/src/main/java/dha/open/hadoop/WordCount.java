7996176704a577c0deabd68e95e68207f177cb6e
Dhanushanth <Dhanushanth@192.168.1.101>
2015-12-11 21:38:16 +0530

    fix.

--- hadoop/src/main/java/dha/open/hadoop/WordCount.java
+++ hadoop/src/main/java/dha/open/hadoop/WordCount.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
