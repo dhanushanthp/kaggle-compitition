a8c106f2eeaf2de40946c1178a236cb88a943969
Dhanushanth <dhanushanthp@gmail.com>
2015-12-15 13:14:42 +0530

    testing.

---
+++ hadoop/src/main/java/dha/open/datacleaning/MyReducer.java
@@ -0,0 +1,20 @@
+package dha.open.datacleaning;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class MyReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	public void reduce(Text arg0, Iterator<IntWritable> arg1, OutputCollector<Text, IntWritable> arg2, Reporter arg3) throws IOException {
+		arg2.collect(arg0, new IntWritable(1));
+	}
+
+}
